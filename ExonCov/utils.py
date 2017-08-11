daba637a47f89cea3274d387d16c7204989a595c
rernst <r.f.ernst-3@umcutrecht.nl>
2017-08-11 10:17:22 +0200

    Add citation

--- ExonCov/utils.py
+++ ExonCov/utils.py
@@ -4,7 +4,10 @@ from sqlalchemy.exc import IntegrityError
 
 
 def get_one_or_create(session, model, create_method='', create_method_kwargs=None, **kwargs):
-    """Get object from database or create if not exist."""
+    """Get object from database or create if not exist.
+
+    Source: http://skien.cc/blog/2014/02/06/sqlalchemy-and-race-conditions-follow-up/
+    """
     try:
         return session.query(model).filter_by(**kwargs).one(), True
     except NoResultFound:
