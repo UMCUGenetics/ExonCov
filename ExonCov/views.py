437152c9c7bb1e04a557bc50963b5fc7af2db64b
rernst <r.f.ernst-3@umcutrecht.nl>
2017-08-17 16:24:46 +0200

    Initial template setup

--- ExonCov/views.py
+++ ExonCov/views.py
@@ -1,7 +1,21 @@
 """ExonCov views."""
 
+from flask import render_template
+
 from ExonCov import app
+from .models import Sample
+
 
 @app.route('/')
-def index():
-    return 'Hello ExonCov V3!'
+@app.route('/sample')
+def samples():
+    """Sample overview page."""
+    samples = Sample.query.all()
+    return render_template('samples.html', samples=samples)
+
+
+@app.route('/sample/<int:id>')
+def sample(id):
+    """Sample page."""
+    sample = Sample.query.get(id)
+    return render_template('sample.html', sample=sample)
