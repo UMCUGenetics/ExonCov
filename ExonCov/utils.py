51e55e7628d63423cde0f0f9f21f448c73ce0061
Robert Ernst <r.f.ernst-3@umcutrecht.nl>
2021-02-19 14:21:57 +0100

    Don't cleanup transcripts or panel

--- ExonCov/utils.py
+++ ExonCov/utils.py
@@ -53,7 +53,7 @@ def weighted_average(values, weights):
 def event_logger(connection, log_model, model_name, action, event_data):
     # Cleanup and transform item to str
     for item in event_data.keys():
-        if item in ['_sa_instance_state', 'user', 'transcripts', 'panel']:
+        if item in ['_sa_instance_state', 'user']:
             event_data.pop(item, None)
         else:
             event_data[item] = str(event_data[item])
