22cf2c23bec4eede210b036905cb6796b094900d
Bas Kasemir <20270816+BasMonkey@users.noreply.github.com>
2023-12-01 15:33:08 +0100

    Added sample lookup by id API endpoint

--- ExonCov/api_views.py
+++ ExonCov/api_views.py
@@ -3,17 +3,10 @@ ExonCov API routes.
 Currently only used to implement api calls for select2 forms (ajax).
 """
 from flask import request, jsonify
-from flask_security import login_required
-from sqlalchemy.orm import joinedload
-from .models import (
-    Sample, SampleProject, SampleSet, SequencingRun, PanelVersion, Panel, CustomPanel, Gene, Transcript,
-    TranscriptMeasurement, panels_transcripts
-)
 
-from . import app, db
 from .auth_middleware import token_required
-from .models import Sample
-from .utils import weighted_average
+from .services import *
+from .utils import model_to_dict
 
 
 @app.route('/api/sample')
@@ -39,60 +32,11 @@ def protected_api():
     return "success"
 
 
-@app.route('/api/sample/<int:id>', methods=['GET'])
+@app.route('/api/sample/<sample_id>')
 @token_required
-def sampleAPI(id):
-    """Sample API"""
-
-    # Query Sample and panels
-    sample = (
-        Sample.query
-        .options(joinedload('sequencing_runs'))
-        .options(joinedload('project'))
-        .options(joinedload('custom_panels'))
-        .get_or_404(id)
-    )
-    query = (
-        db.session.query(PanelVersion, TranscriptMeasurement)
-        .filter_by(active=True)
-        .filter_by(validated=True)
-        .join(Transcript, PanelVersion.transcripts)
-        .join(TranscriptMeasurement)
-        .filter_by(sample_id=sample.id)
-        .order_by(PanelVersion.panel_name)
-        .all()
-    )
-    measurement_types = {
-        'measurement_mean_coverage': 'Mean coverage',
-        'measurement_percentage10': '>10',
-        'measurement_percentage15': '>15',
-        'measurement_percentage30': '>30',
-        'measurement_percentage100': '>100',
-    }
-    panels = {}
-
-    for panel, transcript_measurement in query:
-        if panel.id not in panels:
-            panels[panel.id] = {
-                'description': panel.panel.disease_description_nl,
-                'len': transcript_measurement.len,
-                'name_version': panel.name_version,
-                'coverage_requirement_15': panel.coverage_requirement_15
-            }
-            for measurement_type in measurement_types:
-                panels[panel.id][measurement_type] = transcript_measurement[measurement_type]
-        else:
-            for measurement_type in measurement_types:
-                panels[panel.id][measurement_type] = weighted_average(
-                    values=[panels[panel.id][measurement_type], transcript_measurement[measurement_type]],
-                    weights=[panels[panel.id]['len'], transcript_measurement.len]
-                )
-            panels[panel.id]['len'] += transcript_measurement.len
-
-    results = {"results": {
-            "sample": str(sample),
-            "panels": list(list(panels[panel] for panel in panels)),
-            "measurement_types": list(measurement_types)
-            }
-        }
-    return jsonify(results)
+def sample_by_id_api(sample_id):
+    print("lookup {}".format(sample_id))
+    result = model_to_dict(get_sample_by_id(sample_id))
+    # To omit items from the object:
+    # result.pop("key")
+    return jsonify(result)
